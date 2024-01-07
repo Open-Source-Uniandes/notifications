905c2526757db6308a9fd6d62e95a510b02fa750
Sebastian Urrea <jurrea49@hotmail.com>
2024-01-07 18:47:47 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,9 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('mi-cache').then((cache) => {
       return cache.addAll([
-        'notifications/index.html',
-        'notifications/assets/manifest.json',
-        'notifications/logo.jpeg',
+        '/notifications/index.html',
+        '/notifications/assets/manifest.json',
+        '/notifications/logo.jpeg',
       ]);
     })
   );
