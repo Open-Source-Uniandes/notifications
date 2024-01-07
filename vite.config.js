0c1cb0213696e4c755d3bf30d1f1a7eae350bc2d
Sebastian Urrea <jurrea49@hotmail.com>
2024-01-07 18:25:27 -0500

    preserve assets on deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
+import copy from 'rollup-plugin-copy'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/notifications/'
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'src/assets/*', dest: 'dist/assets' },
+        { src: 'service-worker.js', dest: 'dist' }
+      ],
+      hook: 'writeBundle'
+    })
+  ],
+  base: '/notifications/',
+  build: {
+    assetsDir: 'assets',
+    assetsInlineLimit: 4096,
+    cssCodeSplit: true,
+    target: 'modules',
+    assetsInclude: ['**/*.jpeg', '**/*.png', '**/*.svg', '**/*.jpg', 'service-worker.js'],
+  },
 })
