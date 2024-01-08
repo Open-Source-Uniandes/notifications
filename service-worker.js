65fcd7bc834c25a23b000e1287ca8487841d87a6
Sebastian Urrea <jurrea49@hotmail.com>
2024-01-08 01:44:17 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-// PWA
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('mi-cache').then((cache) => {
@@ -18,33 +17,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
-self.addEventListener("periodicsync", (event) => {
-  // `notifications/${nrc}` es el ID de la tarea periódica
-  const nrc = event.tag.split('/')[1];
-  event.waitUntil(checkAvailability(nrc));
-});
-
-function checkAvailability(nrc) {
-  const apiUrl = `https://ofertadecursos.uniandes.edu.co/api/courses?nameInput=${nrc}`;
-
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      data = data[0];
-      const availableSeats = parseInt(data.maxenrol, 10);
-      const enrolledStudents = parseInt(data.enrolled, 10);
-      const seatsDifference = availableSeats - enrolledStudents;
-
-      if (seatsDifference <= 5) {
-        const notificationOptions = {
-          body: `¡Quedan ${seatsDifference} cupos disponibles para la sección ${data.section} de ${data.class}-${data.course}! (NRC: ${data.nrc})`,
-          icon: '/logo.jpeg',
-        };
-        self.registration.showNotification(`${data.class}-${data.course}: ${data.title}`, notificationOptions);
-      }
-    })
-    .catch(error => {
-      throw new Error('Error fetching data:', error);
-    });
-}
